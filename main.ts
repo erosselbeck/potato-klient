e96b0f2584df1e8a475b2902b6a960bdc82c3daf
isakf2000 <isak.friman2000@gmail.com>
2024-09-18 16:40:07 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,12 @@ function explode () {
     basic.clearScreen()
     basic.showString("GAME OVER")
 }
+function ShowVictoryScreen () {
+    basic.showIcon(IconNames.Target)
+    basic.pause(500)
+    basic.showString("You win!")
+    basic.pause(1000)
+}
 // Mäter om man skakar hårt eller inte.
 input.onGesture(Gesture.EightG, function () {
     strongshake = true
@@ -18,6 +24,29 @@ input.onButtonPressed(Button.A, function () {
     if (gamestate == 1) {
         radio.sendValue(control.deviceName(), -199)
         basic.pause(5000)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (gamestate == 2) {
+        if (havebomb == true) {
+            if (input.buttonIsPressed(Button.AB)) {
+                direction = "forward"
+            } else if (input.buttonIsPressed(Button.B)) {
+                direction = "right"
+            } else if (input.buttonIsPressed(Button.A)) {
+                direction = "left"
+            }
+            // För att förhindra att pausen triggar om man skakar utan att hålla ner en knapp.
+            if (direction != "") {
+                // Pausen finns för att microbiten ska kunna mäta hur hårt man skakar.
+                // 
+                // Eftersom det inte går att nå 6g utan att skaka först, hade koden annars skickat signalen direkt innan den hunnit mäta 6g.
+                basic.pause(500)
+                _throw()
+                direction = ""
+                strongshake = false
+            }
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -46,29 +75,6 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.pause(1000)
         basic.clearScreen()
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    if (gamestate == 2) {
-        if (havebomb == true) {
-            if (input.buttonIsPressed(Button.AB)) {
-                direction = "forward"
-            } else if (input.buttonIsPressed(Button.B)) {
-                direction = "right"
-            } else if (input.buttonIsPressed(Button.A)) {
-                direction = "left"
-            }
-            // För att förhindra att pausen triggar om man skakar utan att hålla ner en knapp.
-            if (direction != "") {
-                // Pausen finns för att microbiten ska kunna mäta hur hårt man skakar.
-                // 
-                // Eftersom det inte går att nå 6g utan att skaka först, hade koden annars skickat signalen direkt innan den hunnit mäta 6g.
-                basic.pause(500)
-                _throw()
-                direction = ""
-                strongshake = false
-            }
-        }
     }
 })
 function _throw () {
@@ -155,6 +161,11 @@ radio.onReceivedValue(function (name, value) {
         gamestate = 2
         led.plot(1, 4)
     } else if (name == "nyrond") {
+    	
+    } else if (name == "newGame") {
+        basic.showString("Restarting")
+        gamestate = 1
+    } else {
     	
     }
 })
